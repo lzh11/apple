acb72bd6b59a3e8d39c57caf8709399b642e8e1c
lzh11 <2239040999@qq.com>
2015-11-11 19:51:36 +0800

    321
    没编完

---
+++ 321.cpp
@@ -0,0 +1,15 @@
+//
+//  321.cpp
+//  apple
+//
+//  Created by LZH on 15/11/11.
+//  Copyright © 2015年 LZH. All rights reserved.
+//
+
+#include "321.hpp"
+int a[4];
+int *p;
+a[0]=10;
+a[1]=20;
+a[2]=30;
+a[3]=40;
---
+++ 321.hpp
@@ -0,0 +1,14 @@
+//
+//  321.hpp
+//  apple
+//
+//  Created by LZH on 15/11/11.
+//  Copyright © 2015年 LZH. All rights reserved.
+//
+
+#ifndef _21_hpp
+#define _21_hpp
+
+#include <stdio.h>
+
+#endif /* _21_hpp */
